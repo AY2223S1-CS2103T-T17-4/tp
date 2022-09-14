bf365fc111fba905df1285cd95d130ad9fc4fd23
NicholasTYD <97309601+NicholasTYD@users.noreply.github.com>
2022-09-14 23:40:35 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -11,6 +11,9 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Remark;
 
+/**
+ * Adds a remark to a person identified using it's displayed index from the address book.
+ */
 public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
